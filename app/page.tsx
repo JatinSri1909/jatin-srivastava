1fdb17ff566dfdfb28c352133e246e7583e5ddb9
Jatin Srivastava <137695680+JatinSri1909@users.noreply.github.com>
2024-10-05 17:53:19 +0530

    Refactor CSS classes and update page content
    - Refactor CSS classes in globals.css to use HSL color values
    - Update page.tsx to change the content displayed

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 
 export default function Home() {
   return (
-    <main className="relative bg-black flex justify-center items-center mx-auto flex-col overflow-hidden sm:px-10 px-5">
-      <div className="max-w-7xl w-full">
-        Jatin Srivastava
+    <main>
+      <div>
+        Jatin
       </div>
     </main>
   );
