d493b15de6c72a3251bdc2c6b51130d1ae16b541
Jatin Srivastava <137695680+JatinSri1909@users.noreply.github.com>
2024-10-07 00:56:54 +0530

    Refactor Globe component and update CSS classes

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,11 +3,13 @@ import { GlobeDemo } from "./Globe";
 
 export function Hero() {
   return (
-    <div className="text-center py-20">
+    <div className="flex flex-col items-center justify-center py-20">
       <h1 className="text-4xl md:text-6xl font-bold text-neutral-800 dark:text-neutral-200">
-        Welcome to My Portfolio
+      Hi,I am Jatin 👋
       </h1>
+      <p className="mt-10 mb-10 ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita rem quam libero. Vitae qui, aliquid temporibus deserunt minus sunt necessitatibus quae, eaque magnam possimus quos maiores, aperiam optio culpa cupiditate? </p>
       <GlobeDemo />
+      <div className="mb-10" />
     </div>
   );
 }
